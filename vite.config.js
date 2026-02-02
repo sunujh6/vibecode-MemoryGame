ad42e372d6bb9b361bd2763e15fc89c658f4d8ef
Jung-Ho Um <jhum@kisti.re.kr>
2026-02-02 18:23:04 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/vibecode-MemoryGame',
+  base:'/vibecode-MemoryGame/',
   plugins: [react()],
   build: { outDir: 'dist', sourcemap: true },
 })
